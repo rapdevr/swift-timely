fb4b0ed5303840432e1a4d7d71de315b3a9cdb77
rapiddevr <77008947+rapdevr@users.noreply.github.com>
2023-12-13 09:02:27 +0000

    created project directory.

---
+++ Timely/Timely/TimelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimelyApp.swift
+//  Timely
+//
+//  Created by Rishi Thiahulan on 13/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TimelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
