e7e29f5442222955c16717df3a33679b675bf1bd
rapiddevr <77008947+rapdevr@users.noreply.github.com>
2024-02-14 12:16:58 +0000

    Updates

---
+++ Timely/Timely/Data/StationNames.swift
@@ -0,0 +1,8 @@
+//
+//  StationNames.swift
+//  Timely
+//
+//  Created by Rishi Thiahulan on 12/02/2024.
+//
+
+import Foundation
---
+++ Timely/Timely/Structs/Graph/StationNode.swift
@@ -0,0 +1,16 @@
+//
+//  stationNode.swift
+//  Timely
+//
+//  Created by Rishi Thiahulan on 25/01/2024.
+//
+
+import Foundation
+
+struct StationNode {
+    var name: String
+    var id: String
+    var servedBy: [String]
+    var connectedTo: [String]
+}
+	
